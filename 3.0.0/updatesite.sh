67c7ba40c5b8eee25c77677f67ee8f13346e6872
jschristie <jschristiein@gmail.com>
2017-04-26 00:33:54 +0530

    updated site & - fixed script and git

--- 3.0.0/updatesite.sh
+++ 3.0.0/updatesite.sh
@@ -1,12 +1,4 @@
 #! /bin/bash
-git pull
+git pull origin 3.0.0
 mkdocs build
-rm -rf 3.0.0/
-mv site/ 3.0.0/
-echo -n "Enter task Performed >"
-read text
-echo "Entered Task: $text"
-
-git add -A
-git commit -m "updated site & - $text"
-git push origin 3.0.0
+echo "SuperGluu Site updated"
